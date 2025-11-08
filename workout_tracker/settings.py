60853965805547b95fc952dd8c1a2a5fd35b7e81
Daniel Favour <feivodanny.06@gmail.como>
2025-11-08 16:06:43 +0100

    updated codebase

--- workout_tracker/settings.py
+++ workout_tracker/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-eb_xti=t^w*(!@y6@+6kkr^ojz5jra4dg2z80=$iqw&9@!r!-&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
