176f759a9d23f96516622a4210c67c36eeeb397f
Daniel Favour <feivodanny.06@gmail.como>
2025-11-12 00:26:14 +0100

    optimsed databse

--- workout_tracker/urls.py
+++ workout_tracker/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path("reports/<uuid:report_id>/exercises/", views.ReportExerciseListCreateAPIView.as_view()),
 
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     # path('api/', include('api.reports.urls')),
 ]
