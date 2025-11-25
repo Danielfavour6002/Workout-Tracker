cc027104efebbaeffdb397d9fb19a5e813654597
Daniel Favour <feivodanny.06@gmail.como>
2025-11-25 22:21:48 +0100

    redeployed to fitlog

--- workout_tracker/settings.py
+++ workout_tracker/settings.py
@@ -168,7 +168,8 @@ SPECTACULAR_SETTINGS = {
     
 }
 CORS_ALLOWED_ORIGINS = [
-    "https://fit-llog.vercel.app",
-    "http://localhost:3000",
+    "http://localhost:3000",           
+    "https://fit-log.vercel.app",     
+    "https://*.v0.app",                 
 ]
 CORS_ALLOW_CREDENTIALS = True
